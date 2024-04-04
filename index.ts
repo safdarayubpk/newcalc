a45388702ef8ae71064b0e796aa808344a46e3a9
safdarayub <safdarayub@gmail.com>
2024-04-04 21:48:05 +0500

    no_one_commit

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! i/usr/bin/env node
+
+import inquirer from "inquirer"
+
+
+let quitions = await inquirer.prompt([
+    {
+        name: "question1",
+        type: "input",
+        message: "Enter the first number: "
+    },
+    {
+        name: "question2",
+        type: "input",
+        message: "Enter the second number: "
+    }
+])
+
+let operator = await inquirer.prompt([
+    {
+        name: "operator",
+        type: "list",
+        message: "Select the operator: ",
+        choices: ["+", "-", "*", "/"]
+    }
+])
+
+if(operator.operator === "+"){
+    console.log(quitions.question1 + parseInt(quitions.question2)) 
+
+}else if(operator.operator === "-"){
+    console.log(quitions.question1 - parseInt(quitions.question2))
+}else if(operator.operator === "*"){
+    console.log(quitions.question1 * parseInt(quitions.question2))
+}else if(operator.operator === "/"){
+    console.log(quitions.question1 / parseInt(quitions.question2))
+}
